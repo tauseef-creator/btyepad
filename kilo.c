61c63484b1aa334483632673050a269a1e64c4c7
tauseef-creator <i211236@nu.edu.pk>
2025-03-27 22:06:46 +0500

    fix raw mode. print the ascii of characters entered

--- kilo.c
+++ kilo.c
@@ -1,24 +1,32 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+
 struct termios orig_termios;
-
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);  //this is to restore the terminal to its original state
-}
-void enableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
-
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);  //this is to turn off the echo and canonical mode
-
+  raw.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 int main() {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
