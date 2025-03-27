4ba4d36d6b2ba406a4f51ef641a3d10ce5465ec3
tauseef-creator <i211236@nu.edu.pk>
2025-03-27 22:28:27 +0500

    Disable Ctrl-S and Ctrl-Q now prints their values IXON

--- kilo.c
+++ kilo.c
@@ -10,13 +10,16 @@ void disableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
+
 void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
+  raw.c_iflag &= ~(IXON);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 
 int main() {
   enableRawMode();
