e7bc795765ae3eb5dc9542bfe8bfbc97e4835371
tauseef-creator <i211236@nu.edu.pk>
2025-03-28 22:43:32 +0500

    add timeout for read().

--- kilo.c
+++ kilo.c
@@ -15,22 +15,29 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-  raw.c_oflag &= ~(OPOST);
-  raw.c_cflag |= (CS8);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);  //miscellaneous flags
+  raw.c_oflag &= ~(OPOST); //output flags
+  raw.c_cflag |= (CS8); //bit mask (sets character size to 8 bits per byte)
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //input, output, control, local flags
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
   enableRawMode();
-  char c;
-   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
   }
+  
   return 0;
 }
