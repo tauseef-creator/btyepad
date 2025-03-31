3edbb8cae06ef99e0ad049fabb60d9dc20b83bb0
tauseef-creator <i211236@nu.edu.pk>
2025-03-31 21:11:15 +0500

    step 42: print welcome

--- kilo.c
+++ kilo.c
@@ -13,6 +13,8 @@
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)  //ascii value of control key is 0x1f. any key can be converted to control key by ANDing with 0x1f
 
+#define KILO_VERSION "0.0.1"
+
 /*** data ***/
 
 struct editorConfig {
@@ -115,7 +117,22 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -126,7 +143,6 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
   abAppend(&ab, "\x1b[?25l", 6);
-  abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
   editorDrawRows(&ab);
   abAppend(&ab, "\x1b[H", 3);
