0306b60b3431807b6aca8944bdaec51654d038ca
tauseef-creator <i211236@nu.edu.pk>
2025-04-03 22:28:36 +0500

    add cursor position tracking and update screen refresh logic

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@
 /*** data ***/
 
 struct editorConfig {
+  int cx, cy;       //cursor position
   int screenrows;    //screen size
   int screencols;    //screen size
   struct termios orig_termios;
@@ -141,12 +142,19 @@ void editorDrawRows(struct abuf *ab) {
 
 
 void editorRefreshScreen() {
-  struct abuf ab = ABUF_INIT;
+   struct abuf ab = ABUF_INIT;
+
   abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[H", 3);
+
   editorDrawRows(&ab);
-  abAppend(&ab, "\x1b[H", 3);
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
+
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
 }
@@ -166,6 +174,8 @@ void editorProcessKeypress() {
 /*** init ***/
 void initEditor() {
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+  E.cx = 0;
+  E.cy = 0;
 }
 
 
