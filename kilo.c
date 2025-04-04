8cfa5622bb7aef3c190d9b1510992675f40fa56f
tauseef-creator <i211236@nu.edu.pk>
2025-04-04 19:43:12 +0500

    capture page up, down escape seq. temporariliy their function is to move cursor to start end of terminal

--- kilo.c
+++ kilo.c
@@ -19,8 +19,11 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
 };
+
 
 /*** data ***/
 
@@ -68,17 +71,30 @@ int editorReadKey() {
     if (nread == -1 && errno != EAGAIN) die("read");
   }
   if (c == '\x1b') {
-    char seq[3];
+    char seq[3];  //some escape sequences are 3 bytes long e.g., in page up/down we have ~ as additional byte
+
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
     if (seq[0] == '[') {
-      switch (seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+        }
       }
     }
+
     return '\x1b';
   } else {
     return c;
@@ -216,11 +232,20 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
-
-    case 'w':
-    case 's':
-    case 'a':
-    case 'd':
+    
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--)
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+      break;
+      
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
       editorMoveCursor(c);
       break;
   }
