2975966d9937526056eb53137ab022fcdd9730ab
tauseef-creator <i211236@nu.edu.pk>
2025-04-08 22:13:07 +0500

    add status bar: inverted bg, line no., filename, no. of lines

--- bytepad.c
+++ bytepad.c
@@ -52,6 +52,7 @@ struct editorConfig {
   int screencols;    //screen size
   int numrows;
   erow *row;
+  char *filename;
   struct termios orig_termios;
 };
 
@@ -220,6 +221,9 @@ void editorAppendRow(char *s, size_t len) {
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
+  free(E.filename);
+  E.filename = strdup(filename);
+
   FILE *fp = fopen(filename, "r");
   if (!fp) die("fopen");
   char *line = NULL;
@@ -307,12 +311,30 @@ void editorDrawRows(struct abuf *ab) {
     }
 
     abAppend(ab, "\x1b[K", 3);
-    if (y < E.screenrows - 1) {
-      abAppend(ab, "\r\n", 2);
-    }
+    abAppend(ab, "\r\n", 2);
   }
 }
 
+void editorDrawStatusBar(struct abuf *ab) {
+  abAppend(ab, "\x1b[7m", 4);
+  char status[80], rstatus[80];
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+    E.filename ? E.filename : "[No Name]", E.numrows);
+  int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+    E.cy + 1, E.numrows);
+  if (len > E.screencols) len = E.screencols;
+  abAppend(ab, status, len);
+  while (len < E.screencols) {
+    if (E.screencols - len == rlen) {
+      abAppend(ab, rstatus, rlen);
+      break;
+    } else {
+      abAppend(ab, " ", 1);
+      len++;
+    }
+  }
+  abAppend(ab, "\x1b[m", 3);
+}
 
 void editorRefreshScreen() {
   editorScroll();
@@ -323,6 +345,7 @@ void editorRefreshScreen() {
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
+  editorDrawStatusBar(&ab);
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);  //this prints the cursor position
@@ -426,7 +449,11 @@ void initEditor() {
   E.rowoff = 0;
   E.coloff = 0;
   E.row = NULL;
+  E.filename = NULL;
+
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+  E.screenrows -= 1;  //to account for the status bar
 }
 
 
