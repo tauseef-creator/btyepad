265d609d9fbbae4d08f6aad9e56af214716b6cb1
tauseef-creator <i211236@nu.edu.pk>
2025-04-12 00:13:25 +0500

    implement new line on clicking enter. (empty + non-empty)

--- bytepad.c
+++ bytepad.c
@@ -213,10 +213,11 @@ void editorUpdateRow(erow *row) {  //copy each character from chars to render
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) return;
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
 
-  int at = E.numrows;
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -274,10 +275,26 @@ void editorRowDelChar(erow *row, int at) {
 /*** editor operations ***/
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {  //if we are at the end of the file, we need to add a new row
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx); //chars on the right of the cursor are passed to the new row
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 void editorDelChar() {
@@ -328,7 +345,7 @@ void editorOpen(char *filename) {
     while (linelen > 0 && (line[linelen - 1] == '\n' ||
                            line[linelen - 1] == '\r'))
       linelen--;
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
 
   free(line);
@@ -543,7 +560,7 @@ void editorProcessKeypress() {
   int c = editorReadKey();
   switch (c) {
     case '\r':
-      /* TODO */
+      editorInsertNewline();
       break;
 
     case CTRL_KEY('q'):
@@ -573,7 +590,7 @@ void editorProcessKeypress() {
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
-    if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+    if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);  //tricks by moving the cursor to the right and then deleting the character
       editorDelChar();
       break;
 
