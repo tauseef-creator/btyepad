cfb9b041144c270ef56923aec38b812de91716b7
tauseef-creator <i211236@nu.edu.pk>
2025-04-20 00:12:36 +0500

    step 149: add escape seq(for hl) in buffer before appending chars

--- bytepad.c
+++ bytepad.c
@@ -38,6 +38,10 @@ enum editorKey {
   PAGE_DOWN
 };
 
+enum editorHighlight {
+  HL_NORMAL = 0,
+  HL_NUMBER
+};
 
 /*** data ***/
 typedef struct erow {
@@ -45,6 +49,7 @@ typedef struct erow {
   int rsize; //size of the row
   char *chars;
   char *render; //rendered version of the row. cause tabs and spaces are not rendered properly in the terminal
+  unsigned char *hl; //highlighting
 } erow;
 
 struct editorConfig {
@@ -178,6 +183,25 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** syntax highlighting ***/
+void editorUpdateSyntax(erow *row) {
+  row->hl = realloc(row->hl, row->rsize);
+  memset(row->hl, HL_NORMAL, row->rsize);
+  int i;
+  for (i = 0; i < row->rsize; i++) {
+    if (isdigit(row->render[i])) {
+      row->hl[i] = HL_NUMBER;  //set hl to number
+    }
+  }
+}
+
+int editorSyntaxToColor(int hl) {  //tells the value of the color
+  switch (hl) {
+    case HL_NUMBER: return 31;
+    default: return 37;
+  }
+}
+
 /*** row operations ***/
 
 
@@ -225,6 +249,8 @@ void editorUpdateRow(erow *row) {  //copy each character from chars to render
 
   row->render[idx] = '\0';
   row->rsize = idx;
+
+  editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len) {
@@ -239,6 +265,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
   
   E.numrows++;
@@ -249,6 +276,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 void editorFreeRow(erow *row) {
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 void editorDelRow(int at) {
   if (at < 0 || at >= E.numrows) return;
@@ -518,16 +546,30 @@ void editorDrawRows(struct abuf *ab) {
       if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
       char *c = &E.row[filerow].render[E.coloff];
+      unsigned char *hl = &E.row[filerow].hl[E.coloff];  
+      int current_color = -1;
       int j;
+
       for (j = 0; j < len; j++) {
-        if (isdigit(c[j])) {
-          abAppend(ab, "\x1b[31m", 5);
+        if (hl[j] == HL_NORMAL) {
+          if (current_color != -1) {
+            abAppend(ab, "\x1b[39m", 5);
+            current_color = -1;
+          }
           abAppend(ab, &c[j], 1);
-          abAppend(ab, "\x1b[39m", 5);
         } else {
+          int color = editorSyntaxToColor(hl[j]);
+          if (color != current_color) {  //to avoid redundant escape sequences
+            current_color = color;
+            char buf[16];
+            int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);  //write esc seq before appending the character
+            abAppend(ab, buf, clen);
+          }
           abAppend(ab, &c[j], 1);
         }
       }
+
+      abAppend(ab, "\x1b[39m", 5);
     }
 
     abAppend(ab, "\x1b[K", 3);
