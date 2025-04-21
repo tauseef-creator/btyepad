7f35317a218ed725a9a5909649afffbf27a54d84
tauseef-creator <i211236@nu.edu.pk>
2025-04-21 23:29:46 +0500

    chore: refactor highlighted serached text color to original state

--- bytepad.c
+++ bytepad.c
@@ -430,6 +430,15 @@ void editorFindCallback(char *query, int key) {
   static int last_match = -1;
   static int direction = 1;
 
+  //save original state of searched text color
+  static int saved_hl_line;
+  static char *saved_hl = NULL;
+  if (saved_hl) {
+    memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+    free(saved_hl);
+    saved_hl = NULL;
+  }
+
   if (key == '\r' || key == '\x1b') {
     last_match = -1;
     direction = 1;
@@ -459,6 +468,10 @@ void editorFindCallback(char *query, int key) {
       E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
+
+      saved_hl_line = current;
+      saved_hl = malloc(row->rsize);
+      memcpy(saved_hl, row->hl, row->rsize);
 
       memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
